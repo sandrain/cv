3ade10bbf540d94a53fe9c56a557929e2257f692
Hyogi Sim <sandrain@gmail.com>
2019-10-22 21:14:26 -0400

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-scp cv/hyogisim-cv-201907.pdf hyogi@rlogin.cs.vt.edu:/home/grads/hyogi/WWW/people.cs.vt.edu
+scp cv/hyogisim-cv-201910.pdf hyogi@rlogin.cs.vt.edu:/home/grads/hyogi/WWW/people.cs.vt.edu
 
